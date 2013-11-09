cf97c7f6eda213c9a02a5282e73ae2b694790742
Adam Wolski <adam.wolski@student.put.poznan.pl>
2013-11-09 23:38:51 +0100

    revert: fixed issues on mobile devices

--- server/chat.c
+++ server/chat.c
@@ -20,7 +20,7 @@
 #define SEM_LOGFILE 2
 
 #define MAX_USERS 20
-#define MAX_ROOMS 4
+#define MAX_ROOMS 10
 
 extern struct Shared *SHM;
 extern int GLOBALsemid;
@@ -39,12 +39,12 @@ char *CHATcreateJSON ( int *a_statusCode, char *a_sender, char *a_room, char *a_
     bzero(aout_reply, 512);
     bzero(messageJSON, messageAloc);
 
-    snprintf(statusJSON, sizeof(statusJSON), " \"status\": %d,", *a_statusCode);
+    snprintf(statusJSON, 32, " \"status\": %d,", *a_statusCode);
     if (a_sender) {
-        snprintf(senderJSON, sizeof(senderJSON), " \"sender\": \"%s\",", a_sender);
+        snprintf(senderJSON, 64, " \"sender\": \"%s\",", a_sender);
     }
     if (a_room) {
-        snprintf(roomJSON, sizeof(roomJSON), " \"room\": \"%s\",", a_room);
+        snprintf(roomJSON, 64, " \"room\": \"%s\",", a_room);
     }
     if (a_message[0] == '[') {
         snprintf(messageJSON, messageAloc, " \"message\": %s", a_message);
@@ -52,7 +52,7 @@ char *CHATcreateJSON ( int *a_statusCode, char *a_sender, char *a_room, char *a_
         snprintf(messageJSON, messageAloc, " \"message\": \"%s\"", a_message);
     }
 
-    snprintf(aout_reply, sizeof(aout_reply), "{ %s %s %s %s }", statusJSON, senderJSON, roomJSON, messageJSON);
+    snprintf(aout_reply, 512, "{ %s %s %s %s }", statusJSON, senderJSON, roomJSON, messageJSON);
 
     free(statusJSON);
     free(senderJSON);
@@ -66,7 +66,7 @@ struct CHATcommand *CHATdecodeCommand(char *a_command, struct CHATcommand *cmd) 
     cmd->name = NULL;
     cmd->param = NULL;
 
-    int paramLen, i = 3, cmdLen = strlen(a_command), k = 0;
+    int paramLen, i = 3, b = 0, cmdLen = strlen(a_command), k = 0;
     char command[16];
     bzero(command, 16);
     while (i <= cmdLen - 1 && !(i < cmdLen - 1 && a_command[i] == '%' && (i + 1 < cmdLen - 1 && a_command[i + 1] == '2') && (i + 2 < cmdLen - 1 && a_command[i + 2] == '0') ) && k <= 15) {
@@ -78,11 +78,10 @@ struct CHATcommand *CHATdecodeCommand(char *a_command, struct CHATcommand *cmd) 
 
     paramLen = cmdLen - i + 1;
     if (paramLen >= 0) {
-        int b = 0;
         cmd->param = malloc(sizeof(char) * paramLen);
         bzero(cmd->param, paramLen);
         bzero(cmd->param, paramLen);
-        i = i + 3;  // remove space -> urldecode(%20)
+        i = i + 3; // remove space -> urldecode(%20)
         while (i < cmdLen) {
             cmd->param[b] = a_command[i];
             i++;
@@ -107,7 +106,7 @@ struct CHATcommand *CHATdecodeCommand(char *a_command, struct CHATcommand *cmd) 
     return cmd;
 }
 
-int CHATisLogged(char *a_name, int *a_soc) {
+int CHATisLogged ( char *a_name, int *a_soc ) {
     int loginPosition = -1, i;
     IPCp(GLOBALsemid, 0);
     for (i = 0; i < MAX_USERS; i++) {
@@ -121,7 +120,7 @@ int CHATisLogged(char *a_name, int *a_soc) {
     return loginPosition;
 }
 
-char *CHATgetUserList(char *aout_list) {
+char *CHATgetUserList (char *aout_list) {
     aout_list = malloc(sizeof(char) * 1024);
     bzero(aout_list, 1024);
     int i;
@@ -140,8 +139,8 @@ char *CHATgetUserList(char *aout_list) {
 }
 
 void CHATsendUserList(int *a_soc) {
-    char *list = NULL;
-    char *reply = NULL;
+    char *list;
+    char *reply;
     int statusCode = 104;
     list = CHATgetUserList(list);
     reply = CHATcreateJSON(&statusCode, NULL, NULL, list, reply);
@@ -152,15 +151,17 @@ void CHATsendUserList(int *a_soc) {
 }
 
 void CHATsendMessage(int type, int *a_soc, char *a_sender, char *a_room, char *a_message) {
-    char *messageJSON = NULL;
+    char *messageJSON;
     int statusCode;
 
     if (type == 0) {
         statusCode = 198;
         messageJSON = CHATcreateJSON(&statusCode, a_sender, a_room, a_message, messageJSON);
+        printf("\npublic:%s\n", messageJSON);
     } else {
         statusCode = 199;
         messageJSON = CHATcreateJSON(&statusCode, a_sender, a_room, a_message, messageJSON);
+        printf("\nprivate:%s\n", messageJSON);
     }
 
     WEBSOCsendMessage(a_soc, messageJSON);
@@ -169,7 +170,7 @@ void CHATsendMessage(int type, int *a_soc, char *a_sender, char *a_room, char *a
 }
 
 void CHATsendCtrlMessage(int *a_soc, char *a_room, char *a_message) {
-    char *messageJSON = NULL;
+    char *messageJSON;
     int statusCode = 195;
     messageJSON = CHATcreateJSON(&statusCode, NULL, a_room, a_message, messageJSON);
     WEBSOCsendMessage(a_soc, messageJSON);
@@ -178,14 +179,14 @@ void CHATsendCtrlMessage(int *a_soc, char *a_room, char *a_message) {
 }
 
 
-void CHATsendUserListToAll() {
+void CHATsendUserListToAll () {
     int users[MAX_USERS], i = 0, j = 0;
     for (i = 0; i < MAX_USERS; i++) {
         users[i] = -1;
     }
 
     i = 0;
-    // get all users
+    //get all users
     IPCp(GLOBALsemid, 0);
     for (j = 0; j < MAX_USERS; j++) {
         if ((*SHM).tabUser[j].fd > 0) {
@@ -194,13 +195,13 @@ void CHATsendUserListToAll() {
         }
     }
     IPCv(GLOBALsemid, 0);
-    // send to all
+    //send to all
     for (j = 0; j < i; j++) {
         CHATsendUserList(&(users[j]));
     }
 }
 
-void CHATremoveRoom(int a_id) {
+void CHATremoveRoom (int a_id) {
     IPCp(GLOBALsemid, 1);
     (*SHM).tabRoom[a_id - 1].id = 0;
     bzero((*SHM).tabRoom[a_id - 1].name, 32);
@@ -209,13 +210,13 @@ void CHATremoveRoom(int a_id) {
     IPCv(GLOBALsemid, 1);
 }
 
-void CHATremoveUserFromActiveRooms(int a_pos, int a_fd) {
-    int i, j, k = 0, l = 0;
+void CHATremoveUserFromActiveRooms ( int a_pos, int a_fd ) {
+    int i, j, k = 0, l = 0, temp;
     int activeRooms[MAX_ROOMS], toRemove[MAX_ROOMS];
 
     IPCp(GLOBALsemid, 0);
     for (j = 0; j < MAX_ROOMS; j++) {
-        int temp = (*SHM).tabUser[a_pos].activeRooms[j];
+        temp = (*SHM).tabUser[a_pos].activeRooms[j];
         if (temp >= 0) {
             activeRooms[k] = temp;
             k++;
@@ -228,7 +229,7 @@ void CHATremoveUserFromActiveRooms(int a_pos, int a_fd) {
         if (activeRooms[i] >= 1 && ((*SHM).tabRoom[activeRooms[i] - 1].users <= 1) && !((*SHM).tabRoom[activeRooms[i] - 1].id == 1)) {
             toRemove[l++] = activeRooms[i];
         } else {
-            // check all users and remove the one
+            //check all users and remove the one
             for (j = 0; j < MAX_USERS; j++) {
                 if ((*SHM).tabRoom[activeRooms[i] - 1].activeUsers[j] == a_fd) {
                     (*SHM).tabRoom[activeRooms[i] - 1].activeUsers[j] = 0;
@@ -244,7 +245,7 @@ void CHATremoveUserFromActiveRooms(int a_pos, int a_fd) {
     }
 }
 
-void CHATsendCtrlMessageToAll(int *a_roomId , char *a_roomName, char *a_message) {
+void CHATsendCtrlMessageToAll (int *a_roomId , char *a_roomName, char *a_message) {
     int users[MAX_USERS], i = 0, k = 0;
     k = CHATgetActiveUsers(a_roomId, users);
 
@@ -254,7 +255,7 @@ void CHATsendCtrlMessageToAll(int *a_roomId , char *a_roomName, char *a_message)
     }
 }
 
-void CHATremoveUser(char *a_name, int *a_soc, int *a_pos) {
+void CHATremoveUser ( char *a_name, int *a_soc, int *a_pos ) {
     int pos = *a_pos, soc = *a_soc;
     char *controlMessage = malloc(sizeof(char) * 128);
     struct CHATcommand room;
@@ -274,7 +275,7 @@ void CHATremoveUser(char *a_name, int *a_soc, int *a_pos) {
     }
     //send to all new list of users
     printf("sending to all \n");
-    snprintf(controlMessage, sizeof(controlMessage), "User %s has logged out.", room.param);
+    snprintf(controlMessage, 128, "User %s has logged out.", room.param);
     CHATsendCtrlMessageToAll(&mainRoom, room.name, controlMessage);
     CHATsendUserListToAll();
     DEBUGprintUsers();
@@ -285,7 +286,7 @@ void CHATremoveUser(char *a_name, int *a_soc, int *a_pos) {
     close(*a_soc);
 }
 
-void CHATassignUser(int *a_pos, int *a_fd, char *a_nick) {
+void CHATassignUser ( int *a_pos, int *a_fd, char *a_nick ) {
     int i;
     IPCp(GLOBALsemid, 0);
     (*SHM).tabUser[*a_pos].fd = *a_fd;
@@ -293,6 +294,7 @@ void CHATassignUser(int *a_pos, int *a_fd, char *a_nick) {
     (*SHM).tabUser[*a_pos].activeRooms[0] = 0;
     for (i = 0; i < MAX_ROOMS; i++) {
         (*SHM).tabUser[*a_pos].activeRooms[i] = -1;
+        //implicitly assign user to main Room (0), because we starting from i=1
     }
     IPCv(GLOBALsemid, 0);
 }
@@ -311,7 +313,7 @@ int CHATfirstEmptySlot() {
 }
 
 
-void CHATsendReply(int a_statusCode, char *a_message, int *a_soc) {
+void CHATsendReply( int a_statusCode, char *a_message, int *a_soc ) {
     char *reply = NULL;
     reply = CHATcreateJSON(&a_statusCode, NULL, NULL, a_message, reply);
     WEBSOCsendMessage(a_soc, reply);
@@ -358,7 +360,7 @@ void DEBUGprintUsers() {
     }
     IPCv(GLOBALsemid, 0);
 }
-void CHATclearActiveRooms(int *a_pos) {
+void CHATclearActiveRooms (int *a_pos) {
     int i = 0;
     IPCp(GLOBALsemid, 0);
     for (i = 0; i < MAX_ROOMS; i++) {
@@ -367,7 +369,7 @@ void CHATclearActiveRooms(int *a_pos) {
     IPCv(GLOBALsemid, 0);
 }
 void CHATloginUser(struct CHATcommand *cmd, int *a_soc) {
-    int firstFree, logged = CHATisLogged(cmd->param, NULL), mainRoom = 0;
+    int firstFree, i, logged = CHATisLogged(cmd->param, NULL), mainRoom = 0;
     char *controlMessage = malloc(sizeof(char) * 128);
     bzero(controlMessage, 128);
     struct CHATcommand room;
@@ -381,13 +383,13 @@ void CHATloginUser(struct CHATcommand *cmd, int *a_soc) {
         if (firstFree >= 0) {
             CHATclearActiveRooms(&firstFree);
             CHATassignUser(&firstFree, a_soc, cmd->param);
-            // notify all users
+            //notify all users
             snprintf(controlMessage, 128, "User %s has logged in.", cmd->param);
             CHATsendCtrlMessageToAll(&mainRoom, room.param, controlMessage);
-            // add to main room
+            //add to main room
             CHATjoinToRoom(&room, a_soc);
             CHATsendReply(101, cmd->param, a_soc);
-            // TO DO send new user lists.
+            //TODO send new user lists.
             CHATsendUserListToAll();
             DEBUGprintUsers();
         } else {
@@ -429,7 +431,7 @@ void CHATexecuteCommand(struct CHATcommand *cmd, int *a_soc) {
     }
 }
 
-int CHATroomExists(char *a_name) {
+int CHATroomExists( char *a_name ) {
     int exists = -1, i = 1;
     IPCp(GLOBALsemid, 1);
     for (i = 0; i < MAX_ROOMS; i++) {
@@ -445,14 +447,13 @@ int CHATroomExists(char *a_name) {
 }
 
 int CHATassignToRoom(int a_id, int *a_fd) {
-    int success = 1;
+    int success = 1, k;
     IPCp(GLOBALsemid, 1);
     if ((*SHM).tabRoom[a_id].users == MAX_USERS) {
         success = 0;
     } else {
-        (*SHM).tabRoom[a_id].id = a_id + 1;  // TO DO
+        (*SHM).tabRoom[a_id].id = a_id + 1; //TODO
         (*SHM).tabRoom[a_id].users++;
-        int k;
         for (k = 0; k < MAX_USERS; k++) {
             if ((*SHM).tabRoom[a_id].activeUsers[k] == 0) {
                 (*SHM).tabRoom[a_id].activeUsers[k] = *a_fd;
@@ -464,7 +465,7 @@ int CHATassignToRoom(int a_id, int *a_fd) {
     return success;
 }
 
-int CHATcreateRoom(char *a_name, int *a_creator) {
+int CHATcreateRoom( char *a_name, int *a_creator ) {
     int freeSlot = -1, i;
     IPCp(GLOBALsemid, 1);
     for (i = 1; i < MAX_ROOMS; i++) {
@@ -484,7 +485,7 @@ int CHATcreateRoom(char *a_name, int *a_creator) {
     return freeSlot;
 }
 
-void CHATuserAddRoom(int *a_pos , int *a_roomPos) {
+void CHATuserAddRoom( int *a_pos , int *a_roomPos ) {
     int i = 0;
     IPCp(GLOBALsemid, 0);
     for (i = 0; i < MAX_ROOMS; i++) {
@@ -496,7 +497,7 @@ void CHATuserAddRoom(int *a_pos , int *a_roomPos) {
     IPCv(GLOBALsemid, 0);
 }
 
-int CHATalreadyInRoom(int a_roomId, int *a_pos) {
+int CHATalreadyInRoom ( int a_roomId, int *a_pos ) {
     int alreadyIn = -1, i = 0;
     IPCp(GLOBALsemid, 0);
     for (i = 0; i < MAX_ROOMS; i++) {
@@ -509,11 +510,12 @@ int CHATalreadyInRoom(int a_roomId, int *a_pos) {
     return alreadyIn;
 }
 
-void CHATleaveRoom(int *a_soc, char *a_name) {
+void CHATleaveRoom( int *a_soc, char *a_name ) {
+    int i = 0, k = 0, users;
     int pos = CHATisLogged(NULL, a_soc);
     int roomPos = CHATroomExists(a_name);
-    if (pos >= 0) {
-        int rpos = CHATalreadyInRoom(roomPos + 1 , &pos);
+    if ( pos >= 0) {
+        int rpos = CHATalreadyInRoom( roomPos + 1 , &pos );
         if (rpos >= 0) {
             if (rpos == 0) {
                 CHATsendReply(505, "You can't leave main room.", a_soc);
@@ -522,7 +524,6 @@ void CHATleaveRoom(int *a_soc, char *a_name) {
                 (*SHM).tabUser[pos].activeRooms[rpos] = -1;
                 IPCv(GLOBALsemid, 0);
                 IPCp(GLOBALsemid, 1);
-                int k = 0, users;
                 for (k = 0; k < MAX_USERS; k++) {
                     if ((*SHM).tabRoom[roomPos].activeUsers[k] == *a_soc) {
                         (*SHM).tabRoom[roomPos].activeUsers[k] = 0;
@@ -541,7 +542,7 @@ void CHATleaveRoom(int *a_soc, char *a_name) {
             CHATsendReply(505, "You are not in that room.", a_soc);
         }
     } else {
-        // not logged
+        //not logged
         perror("user wants to leave the room but he is not logged in ");
     }
 }
@@ -551,8 +552,8 @@ void CHATjoinToRoom(struct CHATcommand *cmd, int *a_soc) {
     int pos = CHATisLogged(NULL, a_soc);
     if (pos >= 0) {
         if (roomPos >= 0) {
+            printf("Room istnieje i dodaje\n");
             if (!(CHATalreadyInRoom(roomPos + 1, &pos) >= 0)) {
-                printf("Room istnieje i dodaje\n");
                 if (CHATassignToRoom(roomPos, a_soc)) {
                     CHATuserAddRoom(&pos, &roomPos);
                     printf("chat add to room: %d\n", roomPos);
@@ -565,6 +566,7 @@ void CHATjoinToRoom(struct CHATcommand *cmd, int *a_soc) {
                 CHATsendReply(503, "You are already in that room.", a_soc);
             }
         } else {
+            //tworzymy room
             roomPos = CHATcreateRoom(cmd->param, a_soc);
             if (roomPos < 0) {
                 CHATsendReply(503, "You cannot join the room. Reached maximum number of rooms.", a_soc);
@@ -586,7 +588,7 @@ void CHATprepareMainRoom() {
     IPCv(GLOBALsemid, 1);
 }
 
-void CHATsendToAll(int *a_roomId , char *a_roomName, int *a_sender, char *a_message) {
+void CHATsendToAll (int *a_roomId , char *a_roomName, int *a_sender, char *a_message) {
     int users[MAX_USERS], i = 0, k = 0;
     k = CHATgetActiveUsers(a_roomId, users);
 
@@ -603,7 +605,7 @@ void CHATsendToAll(int *a_roomId , char *a_roomName, int *a_sender, char *a_mess
     free(nick);
 }
 
-void CHATsendPrivate(int *a_sender, int *a_receiver, char *a_message) {
+void CHATsendPrivate (int *a_sender, int *a_receiver, char *a_message) {
     int fd_receiver, fd_sender;
     char *nick = malloc(sizeof(char) * 32);
     char *nick_r = malloc(sizeof(char) * 32);
@@ -628,7 +630,7 @@ void CHATparseMessage(char *a_message, int *a_soc) {
     int allocated = 1;
     if (strlen(a_message) >= 3) {
         if (a_message[0] == '%' && a_message[1] == '2' && a_message[2] == 'F') {
-            // %40 - urldecode(/)
+            //%40 - urldecode(/)
             cmd = CHATdecodeCommand(a_message, cmd);
             if (cmd->commandId > 0) {
                 CHATexecuteCommand(cmd, a_soc);
@@ -636,7 +638,7 @@ void CHATparseMessage(char *a_message, int *a_soc) {
                 CHATsendCtrlMessage(a_soc, "__CURRENT__", "Unknown command. To see list of available commands type <strong>/help</strong>");
             }
         } else if (a_message[0] == '%' && a_message[1] == '4' && a_message[2] == '0') {
-            // %40 - urldecode(@)
+            //%40 - urldecode(@)
             cmd = CHATdecodeCommand(a_message, cmd);
             int idSender = CHATisLogged(NULL, a_soc);
             int idReceiver = CHATisLogged(cmd->name, NULL);
@@ -650,7 +652,7 @@ void CHATparseMessage(char *a_message, int *a_soc) {
                 CHATsendCtrlMessage(a_soc, "__CURRENT__", "You can't send message to this user because he is not logged in.");
             }
         } else if (a_message[0] == '%' && a_message[1] == '2' && a_message[2] == '5' ) {
-            // %25 - urldecode(%)
+            //%25 - urldecode(%)
             cmd = CHATdecodeCommand(a_message, cmd);
             int idRoom = CHATroomExists(cmd->name);
             int idSender = CHATisLogged(NULL, a_soc);
