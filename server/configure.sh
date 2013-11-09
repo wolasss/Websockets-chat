acd91f4b6fcb05e4cea9f0cddae5984f4bd20f4a
Adam Wolski <adam.wolski@student.put.poznan.pl>
2013-11-09 21:44:00 +0100

    maxdepth 1 for not finding utests folder

--- server/configure.sh
+++ server/configure.sh
@@ -12,19 +12,19 @@ mains_number=`find . -name 'main*.c' | wc -w`
 
 if test $(($mains_number)) -gt 1 
 then
-	mains=`find . -name 'main*.c'`
+	mains=`find . -name 'main*.c' -maxdepth 1`
 	echo -e "Ej ej! Nie oszukuj mnie! Jest więcej niż jeden main: \n\n $mains \n\n Którego mam kompilować?"
 	read main
 	echo "A zatem $main będzie się radośnie kompilował."
 else
-	main=`find . -name 'main*.c'`
+	main=`find . -name 'main*.c' -maxdepth 1`
 fi
 
 touch Makefile
 clr_main=`basename $main | cut -d\. -f1` 
 main_gpp=`gcc -MM $main -g -Wall -pthread`
 
-objekty=`find . -name "*.c" | grep -v "main" | sed "s/\.c/\.o/g" | tr '\n' ' ' `
+objekty=`find . -name "*.c" -maxdepth 1 | grep -v "main" | sed "s/\.c/\.o/g" | tr '\n' ' ' `
 echo $objekty;
 echo -e "all: $projectname\n\n$projectname: $clr_main.o $objekty\n\tgcc $clr_main.o $objekty -o $projectname -pthread -g -lcrypto -Wall\n" > Makefile
 
@@ -32,7 +32,7 @@ elem_main=`gcc -MM $main -Wall`
 echo -e "$elem_main" >> Makefile
 echo -e "\tgcc -c $clr_main.c -o $clr_main.o -lcrypto -g -Wall -pthread\n" >> Makefile
 
-for parameter in `find . -name "*.c" | grep -v "main"` 
+for parameter in `find . -name "*.c" -maxdepth 1 | grep -v "main"` 
 do
 	elem=`gcc -MM $parameter`
 	clr_param=`basename $parameter | cut -d\. -f1`
