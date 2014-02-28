0ccf3cea1a8b7b70bc34500657fe0354ecd2f0f7
Adam Wolski <wolas.abc@gmail.com>
2014-02-28 23:37:04 +0100

    grunt compass & clean

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -3,7 +3,19 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+    clean: ['js/build', 'css'],
     uglify: {
+      options: {
+        mangle: true,
+        compress: true,
+      },
       build: {
         src: ['js/vendor/*.js', 'js/plugins.js', 'js/sandbox.js', 'js/modules/*.js', 'js/core.js'],
         dest: 'js/build/app.min.js'
@@ -12,6 +24,10 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default', ['uglify']);
+
+
+  grunt.registerTask('default', ['compass', 'uglify']);
 };
